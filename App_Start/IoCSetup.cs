2ae1a42fd3d81ca3329550027a1c76ad36a69060
Andy Wahrenberger <andywahr@microsoft.com>
2019-06-27 21:44:28 -0500

    updated with mapping fun

---
+++ App_Start/IoCSetup.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace fullframework_webapi_in_container
+{
+    public static class IoC
+    {
+        public static void Setup(HttpConfiguration config)
+        {
+            var builder = new ContainerBuilder();
+
+            // Register Mediatr
+
+            builder.RegisterType<Mediator>()
+                   .As<IMediator>()
+                   .InstancePerLifetimeScope();
+
+            // request & notification handlers
+            builder.Register<ServiceFactory>(context =>
+            {
+                var c = context.Resolve<IComponentContext>();
+                return t => c.Resolve(t);
+            });
+
+            // setup WebApi controllers for IoC
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // register items in other assembly
+            builder.RegisterAssemblyModules(typeof(Application.Module).Assembly);
+
+            WebApiApplication.Container = builder.Build();
+
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(WebApiApplication.Container);
+        }
+    }
+}
--- Global.asax.cs
+++ Global.asax.cs
@@ -1,12 +1,20 @@
-﻿using System.Web.Http;
+﻿using Autofac;
+using System.Web.Http;
 
 namespace fullframework_webapi_in_container
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        public static IContainer Container;
+
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configure((config) => 
+                {
+                    IoC.Setup(config);
+                    WebApiConfig.Register(config);
+                }
+            );
         }
     }
 }
